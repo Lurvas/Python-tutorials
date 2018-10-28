44813bf6c18645577ba0fd0b56eb329fb44ee38b
Lurvas <kristianeng@hotmail.com>
2018-10-28 19:40:17 +0100

    add method that checks guessed number against th choosen number

--- guess_number.py
+++ guess_number.py
@@ -10,15 +10,25 @@ def main():
             end = int(input("Input end number:\n"))
             choosen_number = random_number(start, end)
             print(choosen_number)
-            gueuss = input("A number between " + str(start - 1) + " and " + str(end + 1) + " has been choosen, it's time to guess:\n")
+            guess = input("A number between " + str(start - 1) + " and " + str(end + 1) + " has been choosen, it's time to guess:\n")
         except ValueError:
-            print("Input must be a number!")
+            print("Input must be a number!") 
              
 
 #Define function that generate a number between a and b
 def random_number(a, b):
     rand_int = random.randint(a, b)
     return rand_int
+
+def check_number(choosen_number, guess):
+    if guess == choosen_number:
+        return True
+    elif guess < choosen_number:
+        print("Your guess was to low, try again!")
+        return False
+    elif guess > choosen_number:
+        print("Your guess was to high, try again!")
+        return False
 
 if __name__ == "__main__":
     main()

6af284ccf07acafa474b47e7f339cb48ad10a629
Lurvas <kristianeng@hotmail.com>
2018-11-03 14:45:56 +0100

    initial commit of people_in_space.py

---
+++ people_in_space.py
@@ -0,0 +1,9 @@
+import json
+import tkinter
+import turtle
+import urllib.request
+
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print(result)

733702427aa400c8019b0fa259c40cc34fb1fce5
Lurvas <kristianeng@hotmail.com>
2018-11-20 18:08:02 +0100

    add people_in_space.py for review

--- people_in_space.py
+++ people_in_space.py
@@ -1,9 +1,30 @@
 import json
-import tkinter
 import turtle
 import urllib.request
+from PIL import Image
 
 url = "http://api.open-notify.org/astros.json"
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
-print(result)
+
+print("People in Space: ", result["number"])
+
+people = result["people"]
+
+for p in people:
+    print(p["name"] + " in " + p["craft"])
+
+url = "http://api.open-notify.org/iss-now.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+location = result["iss_position"]
+lat = location["latitude"]
+lon = location["longitude"]
+print("Latitude: ", lat)
+print("Longitude: ", lon)
+
+screen = turtle.Screen()
+screen.setup(720, 360)
+screen.setworldcoordinates(-180, -90, 180, 90)
+#screen.bgpic("map.png")
